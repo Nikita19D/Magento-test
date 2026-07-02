f88b01c195716071d25c82a494592d00ca1c0982
Nikita Husarenko <gusarenkonikita1@gmail.com>
2026-07-02 01:16:32 +0100

    Signed-off-by: Nikita Husarenko <gusarenkonikita1@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
 from google import genai
 
 load_dotenv()
-client = genai.Client(api_key=os.getenv("API_KEY"))
+
+app=FastAPI()
+
+items=[]
+
+
+@app.get ("/")
+def root():
+    return{"Hello":"World"}
+
+@app.post("/items")
+def create_item(item:str):
+    items.append(item)
+    return item
+
+
+
+"""client = genai.Client(api_key=os.getenv("API_KEY"))
 
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
-    input="give me a latin quote and its translation"
+    input="write a latin quote and a english traslation of it"
+    #input="Create a simple html magento sample"
 )
-print(interaction.output_text)
+print(interaction.output_text)"""
