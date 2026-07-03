0825a1c30b7558fce88c1c562d3b995ccd8743bc
Nikita Husarenko <gusarenkonikita1@gmail.com>
2026-07-03 16:19:49 +0100

    Simple update on FastAPI code

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from google import genai
 
 load_dotenv()
 
 app=FastAPI()
+
+class Item(BaseModel):
+    text:str=None
+    is_done:bool = False
 
 items=[]
 
@@ -14,10 +19,24 @@ items=[]
 def root():
     return{"Hello":"World"}
 
-@app.post("/items")
-def create_item(item:str):
+@app.post("/items") 
+def create_item(item:Item):
     items.append(item)
-    return item
+    return items
+
+@app.get("/items", response_model=list[Item]) 
+def list_items(limit:int=10):
+    return items[0:limit]
+
+
+@app.get ("/items/{item_id}", response_model=Item)
+def get__item(item_id:int) -> Item:
+    if item_id<len(items):
+        item =items[item_id]
+        return item
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
 
 
 """client = genai.Client(api_key=os.getenv("API_KEY"))
